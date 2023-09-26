e90cea6e8eb265f991883412dfd79b654a4bb7d6
Santfogu <79651297+Santfogu@users.noreply.github.com>
2023-09-26 15:56:01 -0500

    Actividad localización

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
 
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+
+const userLanguage = navigator.language || 'en'; 
+
+const languageToMessages = {
+  'en': { locale: 'en', messages: localeEnMessages },
+  'es': { locale: 'es', messages: localeEsMessages },
+};
+
+const selectedLanguage = languageToMessages[userLanguage] || languageToMessages['en'];
+
+ReactDOM.render(
+    <IntlProvider locale={selectedLanguage.locale} messages={selectedLanguage.messages}>
+        <JobsList locale={selectedLanguage.locale} />
+    </IntlProvider>, document.getElementById("root")
+);
